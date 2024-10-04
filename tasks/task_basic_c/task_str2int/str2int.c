c63fbece3808e85157076fea96f85886b3aefd01
Dmitriy Bragin <brdm.06@yandex.ru>
2024-10-04 22:52:02 +0300

    Finished

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,10 +1,37 @@
 #include <assert.h>
+#include <limits.h>
 #include "str2int.h"
 
 
 int str2int(const char *str) {
+    
+    int sign = 1;
+    
+    long long result = 0;
+
+    \
     if (*str == '-') {
-        assert(1 == 0);
+        sign = -1;
+        str++;
     }
-    return 0;
+    else if (*str == '+') {
+        str++;
+    }
+    
+    assert(*str != '\0');
+    
+    while (*str != '\0') {
+       
+        assert('0' <= *str && *str <= '9');
+
+        
+        result = result * 10 + (*str - '0');
+       
+        str++;
+        
+        assert((sign == 1 && result <= (long long)INT_MAX) || (sign == -1 && (-result) >= (long long)INT_MIN));
+    }
+
+    
+    return (int)(sign * result);
 }
