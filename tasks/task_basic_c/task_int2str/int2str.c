deddb285f53294593f7bd2d047a33d513cf9b076
Dmitriy Bragin <brdm.06@yandex.ru>
2024-10-27 16:00:17 +0300

    completed tests

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,24 @@
-#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
 #include "int2str.h"
-#include "stdio.h"
-
-char* int2str(int number) {
-    char* str = "0";
+char* int2str(int num) {
+    if (num == -2147483648) {
+        return "-2147483648";
+    }
+    int sign = num < 0 ? -1 : 0;
+    num = abs(num);
+    int size = num == 0 ? 1 : (int)log10(num) + 1; 
+    size += sign == -1 ? 1 : 0;  
+    char* str = (char*)malloc(sizeof(char) * size + 13);
+    int i = 0;
+    do {
+        str[size - 1 - i++] = num % 10 + '0';
+        num /= 10;
+    } while (num > 0);
+    if (sign == -1) {
+        str[0] = '-';
+    }
+    str[size] = '\0';
+    
     return str;
 }
